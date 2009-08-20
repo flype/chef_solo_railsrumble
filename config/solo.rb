5f07937d3e513830fe2f96575390e0d3adc207bd
retr0h <john@dewey.ws>
2009-08-20 13:52:37 -0700

    Removed file_store_path and file_cache_path

--- config/solo.rb
+++ config/solo.rb
@@ -1,8 +1,4 @@
-##
-# Example Chef Solo Config
 cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
 log_level         :info
-file_store_path   File.join(File.dirname(__FILE__), '..')
-file_cache_path   File.join(File.dirname(__FILE__), '..')
 
 Chef::Log::Formatter.show_time = false
